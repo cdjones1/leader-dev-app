63e398a59a541e0c7646567ced27abfa14055466
cdjones1 <codyjonescfa@gmail.com>
2026-08-24 23:22:53 -0500

    Delete src directory

--- src/db.js
+++
@@ -1,7 +0,0 @@
-// This file creates ONE connection to the database that the rest
-// of the app reuses, instead of opening a new connection every time.
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
-
-module.exports = prisma;
